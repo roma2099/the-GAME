9ff7d2168e676de693e1ca110d3d78623a70b8fa
rbrito@uta.cv <you@example.com>
2021-01-08 19:44:36 -0100

    unshited LOL

--- main.py
+++ main.py
@@ -6,9 +6,6 @@
 import pygame, player, sys, os, tile, enemy, accessorie, pickle
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 def img_load(file_directory, scale):
